e6a630da053edcf27adbcca207951585a2801838
veraph <jmw3531@live.com>
2020-06-30 21:51:30 +0100

    add the 8th.

---
+++ math/8_atoi.cc
@@ -0,0 +1,31 @@
+// atoi.cc -- change string to integer
+#include <iostream>
+#include <string>
+#include <cctype>
+using std::string;
+
+int main(string str)
+{
+    long result = 0;
+    int indicator = 1;
+
+    int n = str.find_first_not_of(' ');
+    if (str[n] == '-') {
+        indicator =  -1;
+        ++n;
+    } else if (str[n] == '+') {
+        indicator = 1;
+        ++n;
+    }
+        
+    while (isdigit(str[n])) {
+        result = result * 10 + str[n];
+        ++n;
+        if (result*indicator >= INT32_MAX)
+            return INT32_MAX;
+        if (result*indicator <= INT32_MIN)
+            return INT32_MIN;
+    }
+
+    return result * indicator;
+}
