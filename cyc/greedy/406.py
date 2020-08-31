024c9fc8d20ca667bca5a35219b9f29ed0372577
Veraph <jmw3531@live.com>
2020-08-31 21:35:20 +0800

    add some file related with greedy.

---
+++ cyc/greedy/406.py
@@ -0,0 +1,33 @@
+# 406.py -- Queue Reconstruction by Height
+
+'''
+Description:
+Suppose you have a random list of people standing in a queue. 
+Each person is described by a pair of integers (h, k), 
+where h is the height of the person and k is the number of people in front of this person who have a height greater than or equal to h. 
+Write an algorithm to reconstruct the queue.
+
+Note:
+The number of people is less than 1,100.
+
+ 
+Example
+Input:
+[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+
+Output:
+[[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+'''
+
+def reconstructQueue(people):
+    '''
+    sort people with decreasing height first,
+    if have same height, sort with increasing index instead.
+    then insert each elements with their index one by one,
+    later elements insertion wont influence the previous elements beacause bigger one inserted already.
+    '''
+    people.sort(key=lambda x: (-x[0], x[1]))
+    res = []
+    for p in people:
+        res.insert(p[1], p)
+    return res
---
+++ cyc/greedy/452.py
@@ -0,0 +1,38 @@
+# 452.py -- Minimum Number of Arrows to Burst Balloons
+
+'''
+Description:
+There are a number of spherical balloons spread in two-dimensional space. 
+For each balloon, provided input is the start and end coordinates of the horizontal diameter. 
+Since it's horizontal, y-coordinates don't matter and hence the x-coordinates of start and end of the diameter suffice. 
+Start is always smaller than end. There will be at most 104 balloons.
+
+An arrow can be shot up exactly vertically from different points along the x-axis. 
+A balloon with xstart and xend bursts by an arrow shot at x if xstart ≤ x ≤ xend. 
+There is no limit to the number of arrows that can be shot. An arrow once shot keeps travelling up infinitely. 
+The problem is to find the minimum number of arrows that must be shot to burst all balloons.
+
+Example:
+Input:
+[[10,16], [2,8], [1,6], [7,12]]
+Output:
+2
+
+Explanation:
+One way is to shoot one arrow for example at x = 6 (bursting the balloons [2,8] and [1,6]) and another arrow at x = 11 (bursting the other two balloons).
+'''
+
+def findMinArrowShots(points):
+    '''
+    the difference from 435 is that
+    [1,2] and [2,3] are overlapping
+    '''
+    points.sort(key=lambda x: x[1])
+    end, cnt = float('-inf'), 0
+    for b, e in points:
+        if b > end:
+            end = e
+        else:
+            cnt += 1
+    return len(points) - cnt
+
