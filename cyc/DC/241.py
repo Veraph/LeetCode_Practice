730a2260b63856b706831aac9525f81512841aa6
Veraph <jmw3531@live.com>
2020-09-01 21:08:28 +0800

    finish DC code.

---
+++ cyc/DC/241.py
@@ -0,0 +1,51 @@
+# 241.py -- Different Ways to Add Parentheses
+
+'''
+Description:
+Given a string of numbers and operators, 
+return all possible results from computing all the different possible ways to group numbers and operators. 
+The valid operators are +, - and *.
+
+Example 1:
+Input: "2-1-1"
+Output: [0, 2]
+Explanation: 
+((2-1)-1) = 0 
+(2-(1-1)) = 2
+
+Example 2:
+Input: "2*3-4*5"
+Output: [-34, -14, -10, -10, 10]
+Explanation: 
+(2*(3-(4*5))) = -34 
+((2*3)-(4*5)) = -14 
+((2*(3-4))*5) = -10 
+(2*((3-4)*5)) = -10 
+(((2*3)-4)*5) = 10
+'''
+
+def diffWaysToCompute(input):
+    '''
+    use the thinking of divide and conquer.
+    divide the parent problem into sub problems and solve.
+    '''
+    if input.isdigit():
+        return [int(input)]
+    res = []
+    for i in range(len(input)):
+        if input[i] in '-+*':
+            res1 = diffWaysToCompute(input[:i])
+            res2 = diffWaysToCompute(input[i+1:])
+            for j in res1:
+                for k in res2:
+                    res.append(helper(j,k,input[i]))
+    return res
+
+def helper(m,n,op):
+    if op == '-':
+        return m-n
+    elif op == '+':
+        return m+n
+    else:
+        return m*n
+diffWaysToCompute('2*3-4*5')
---
+++ cyc/DC/95.py
@@ -0,0 +1,50 @@
+# 95.py -- Unique Binary Search Trees II
+
+'''
+Description:
+Given an integer n, generate all structurally unique BST's (binary search trees) 
+that store values 1 ... n.
+
+Example:
+Input: 3
+Output:
+[
+  [1,null,3,2],
+  [3,2,null,1],
+  [3,1,null,null,2],
+  [2,1,3],
+  [1,null,2,null,3]
+]
+
+Explanation:
+The above output corresponds to the 5 unique BST's shown below:
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+ 
+
+Constraints:
+0 <= n <= 8
+'''
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def generateTrees(n):
+    def generate(l, r):
+        if l == r:
+            return [None]
+        nodes = []
+        for i in range(l, r):
+            for ltree in generate(l, i):
+                for rtree in generate(i+1, r):
+                    node = TreeNode(i+1, ltree, rtree)
+                    nodes.append(node)
+        return nodes
+    return generate(0, n) if n else []
+
