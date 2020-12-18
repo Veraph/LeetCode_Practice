3bfff6046d7b10c1361e747e06e081d77920b490
veraph <jmw3531@live.com>
2020-12-18 19:52:56 +0800

    update.

--- lc/714.cpp
+++ lc/714.cpp
@@ -17,4 +17,34 @@ class Solution {
     int maxProfit2(vector<int> &prices, int fee) {
         int sz = prices.size();
         vector<int> dp(2);
-        dp[0] = 0;
+        dp[0] = 0, dp[1] = -prices[0];
+        for (int i = 0; i < sz; i++) {
+            int temp = dp[0];
+            dp[0] = max(dp[0], dp[1] + prices[i] - fee);
+            dp[1] = max(dp[1], temp - prices[i]);
+        }
+        return dp[0];
+    }
+};
+
+// greedy
+class Solution {
+public:
+    int maxProfit3(vector<int> &prices, int fee) {
+       int profit = 0;
+       int minPrice = prices[0];
+
+       for (int i = 1; i < prices.size(); i++) {
+           // really sell and but a new one
+           if (prices[i] < minPrice) minPrice = prices[i];
+
+           // continually gain, not really sell
+           // add up the profit of every single day
+           if (prices[i] > minPrice + fee) {
+               profit += prices[i] - minPrice - fee;
+               minPrice = prices[i] - fee;
+           }
+       }
+       return profit;
+    }
+};
