531cd5d5390de1f649b88737d7a1d2fdfc07be55
veraph <jmw3531@live.com>
2021-03-09 10:04:41 +0800

    lc daily.

---
+++ lc/1047.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string stk;
+        for (char c : s) {
+            if (!stk.empty() && stk.back() == c) stk.pop_back();
+            else stk.push_back(c);
+        }
+        return stk;
+    }
+
+};
